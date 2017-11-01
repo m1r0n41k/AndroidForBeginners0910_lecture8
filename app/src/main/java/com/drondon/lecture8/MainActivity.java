a258f4de482da3fe7002f2fdf925525ca6c1ebf4
Andrii Miroshnychenko <collosteam@gmail.com>
2017-11-01 20:41:31 +0200

    Added menu

--- app/src/main/java/com/drondon/lecture8/MainActivity.java
+++ app/src/main/java/com/drondon/lecture8/MainActivity.java
@@ -1,7 +1,12 @@
 package com.drondon.lecture8;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +14,32 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        switch (itemId) {
+            case R.id.action_settings:
+                //Навигация в настройки
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+
+            case R.id.action_about_us:
+                //Навигация в описание продукта
+                Toast.makeText(this, "About us", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
